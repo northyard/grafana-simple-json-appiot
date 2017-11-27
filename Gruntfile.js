8f3e6113548dae04ce5473812551286d4e0adbf5
northyard <34015585+northyard@users.noreply.github.com>
2017-11-27 10:37:29 +0800

    first import
    first import

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+
+    clean: ["dist"],
+
+    copy: {
+      src_to_dist: {
+        cwd: 'src',
+        expand: true,
+        src: ['**/*', '!**/*.js', '!**/*.scss'],
+        dest: 'dist'
+      },
+      pluginDef: {
+        expand: true,
+        src: ['README.md'],
+        dest: 'dist'
+      },
+      node_modules: {
+        cwd: 'node_modules',
+        expand: true,
+        src: ['**/axios/dist/*'],
+        dest: 'dist/node_modules'
+      },
+    
+    },
+
+
+    watch: {
+      rebuild_all: {
+        files: ['src/**/*'],
+        tasks: ['default'],
+        options: {spawn: false}
+      }
+    },
+
+   
+  });
+
+  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:node_modules']);
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+## Simple JSON Datasource For AppIoT2.0
+
+More documentation about datasource plugins can be found in the [Docs](https://github.com/grafana/grafana/blob/master/docs/sources/plugins/developing/datasources.md).
+
+## Installation
+
+To install this plugin using the `grafana-cli` tool:
+```
+sudo grafana-cli plugins install grafana-simple-json-datasource-appiot
+sudo service grafana-server restart
+```
+See [here](https://grafana.com/plugins/grafana-simple-json-datasource/installation) for more
+information.
+
